88c612105dcfc2b336dbdd90badf3820b7c956a5
Joey Padasian <joey.padasian@jellyfish.co.uk>
2018-04-19 16:21:07 +0100

    create: project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -6,49 +6,75 @@ import Footer from '../components/footer';
 import Placeholder from '../components/placeholder';
 import './project.scss';
 
-const Project = props => {
-  const { slug } = props.pathContext;
-  const postNode = props.data.markdownRemark;
-  const project = postNode.frontmatter;
-  const currentBanner = project.banners[0];
-  const basePath = `/projects/${slug}/`;
-
-  const activateLasers = element => {
-    console.log(element);
+const BannerLink = props => {
+  const handleClick = () => {
+    props.onHeaderClick(props);
   };
-
   return (
-    <div className="content">
-      <Header />
-      <div className="project">
-        <div className="sidebar">
-          <h2 className="title">{project.title}</h2>
-          <p className="description">{project.description}</p>
-          <div className="details">
-            <h3>Tags</h3>
-            <p>{project.tags.join(', ')}</p>
-          </div>
-          <div className="banners">
-            <h3>Banners</h3>
-            <ul>
-              {project.banners.map(banner => (
-                <li key={kebabCase(banner.name)}>
-                  <a href="" onClick={activateLasers} onKeyPress={activateLasers} role="button">
-                    {banner.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="main">
-          <Placeholder basePath={basePath} banner={currentBanner} />
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <li>
+      <button onClick={handleClick} onKeyPress={handleClick}>
+        {props.name}
+      </button>
+    </li>
   );
 };
+
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
+    const { slug } = props.pathContext;
+    const postNode = props.data.markdownRemark;
+    const project = postNode.frontmatter;
+    this.state = {
+      title: project.title,
+      description: project.description,
+      tags: project.tags,
+      banners: project.banners,
+      basePath: `/projects/${slug}/`,
+      currentBanner: project.banners[0],
+    };
+  }
+
+  updateBanner(bannerName) {
+    this.setState({
+      currentBanner: this.state.banners.find(x => x.name === bannerName),
+    });
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <Header />
+        <div className="project">
+          <div className="sidebar">
+            <h2 className="title">{this.state.title}</h2>
+            <p className="description">{this.state.description}</p>
+            <div className="details">
+              <h3>Tags</h3>
+              <p>{this.state.tags.join(', ')}</p>
+            </div>
+            <div className="banners">
+              <h3>Banners</h3>
+              <ul>
+                {this.state.banners.map(banner => (
+                  <BannerLink
+                    key={kebabCase(banner.name)}
+                    name={banner.name}
+                    onHeaderClick={() => this.updateBanner(banner.name)}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="main">
+            <Placeholder basePath={this.state.basePath} banner={this.state.currentBanner} />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Project;
 
